73d430e62b6bb1a9158ae551b8c6e841823753bb
Bannaphon <112239420+Bannaphon@users.noreply.github.com>
2022-10-08 16:01:52 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images/day6-.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, {{name}}</h3>
+        <span>{{motto}}</span>
+        <p>{{content}}</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+  data() {
+      return {
+        name: 'Bannaphon Kongthip',
+        motto: 'Fortune favors the prepared mind.',
+        content: 'im study at Bankok University in Engineer',
+      }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
